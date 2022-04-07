498594fa3064f069ba76f51eae725252d587fbb7
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-04-07 12:39:13 -0500

    Agregamos el puerto 4000 para que el servidor escuche por ese puerto y app.use para que al consultarlo nos devuelva un hola mundo

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-import { Express } from "express";
+import express from "express";
 
+const app = express();
+
+app.use("/", (req, res) => {
+    res.send("Hola Mundo");
+})
+
+app.listen(4000, () => {
+    console.log('Servidor funcionando en el puerto 4000');
+});
