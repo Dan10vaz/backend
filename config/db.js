be9b455f978affcbb0e186eca8da0cc3beab9082
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-04-07 17:42:45 -0500

    COnfiguramos las variables de entorno para que sea mas segurida la base de datos, pasamos la conexion al index.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
     try {
-        const db = await mongoose.connect("mongodb+srv://dan20051995:dan20051995@cluster0.mnzoj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+        const db = await mongoose.connect(process.env.MONGO_URI, 
         {
             useNewUrlParser: true,
             useUnifiedTopology: true
