f674c3d7cf9feaac4a258608b817bebd807efd37
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-04-11 10:20:34 -0500

    enviamos informacion de correo, password y nombre por tipo post y no genero una respuesta json

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,6 +1,10 @@
 
 
 const registrar = (req, res) =>{
+    const {nombre, email, password} = req.body;
+    console.log(nombre);
+    console.log(email);
+    console.log(password);
     res.json({msg: "Registrando usuario..."});
 };
 
