0238e67ccae67cdc8e05d2a10818c857e7a15050
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-04-11 14:02:42 -0500

    creamos el primer usuario en la base de datos

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,18 +1,21 @@
+import Veterinario from "../models/Veterinario.js";
 
+const registrar = async (req, res) => {
+  /* const {nombre, email, password} = req.body; */
 
-const registrar = (req, res) =>{
-    const {nombre, email, password} = req.body;
-    console.log(nombre);
-    console.log(email);
-    console.log(password);
-    res.json({msg: "Registrando usuario..."});
+  try {
+    //Guardar un Nuevo Veterinario
+    const veterinario = new Veterinario(req.body);
+    const veterianrioGuardado = await veterinario.save(); //creamos o guardamos en la abse de datos
+
+    res.json(veterianrioGuardado);
+  } catch (error) {
+    console.log(`El error es: ${error}`);
+  }
 };
 
 const perfil = (req, res) => {
-    res.json({msg: "Mostrando perfil"});
+  res.json({ msg: "Mostrando perfil" });
 };
 
-export {
-    registrar,
-    perfil
-};
+export { registrar, perfil };
