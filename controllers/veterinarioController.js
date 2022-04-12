12a7d0cb64e75849027b8213fddcdaa86e9bb82d
DanielGesto <danielcristophervazquezayala@gmail.com>
2022-04-12 16:19:07 -0500

    validamos que no se puedan repetir usuarios por medio del correo electronico y lanzamos un mensaje de error status 404

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -4,7 +4,13 @@ const registrar = async (req, res) => {
   const {email} = req.body;
 
   //Prevenir usuarios duplicados
-    const existeUsuario = await Veterinario
+    const existeUsuario = await Veterinario.findOne({email});
+
+    if(existeUsuario) {
+      const error = new Error('Usuario ya registrado');
+      return res.status(400).json({msg: error.message});
+    }
+
   try {
     //Guardar un Nuevo Veterinario
     const veterinario = new Veterinario(req.body);
