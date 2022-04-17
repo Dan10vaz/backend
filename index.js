0968d04ae342b12b7038d910e761a7d413583ded
Daniel Vaz <dcva9565@hotmail.com>
2022-04-17 15:45:53 -0500

    Creamos el routing de los pacientes y le agregamos las funciones dle controlador, tambien cremaos en el index la ruta principal hacia pacientes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import conectarDB from "./config/db.js";
-import veterinarioRoutes from "./routes/veterinarioRoutes.js"
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacientesRoutes from "./routes/pacienteRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,10 +10,10 @@ dotenv.config();
 conectarDB();
 
 app.use("/api/veterinarios", veterinarioRoutes);
+app.use("/api/pacientes", pacientesRoutes);
 
-
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor funcionando en el puerto ${PORT}`);
-});      
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
