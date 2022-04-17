adf510381be82c1800f66c8044944ac801ef3e94
Daniel Vaz <dcva9565@hotmail.com>
2022-04-17 15:22:46 -0500

    creamos el modelo de pacientes y su estructura

---
+++ models/Paciente.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const pacienteSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String, // de tipo string
+      require: true, // es obligatorio
+    },
+    propietario: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    fecha: {
+      type: Date,
+      require: true,
+    },
+    sintomas: {
+      type: String,
+      require: true,
+    },
+    veterinario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Veterinario",
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Paciente = mongoose.model("Paciente", pacienteSchema);
+
+export default Paciente;
