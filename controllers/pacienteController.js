488fc8481ac84fb0e36f132d250b3288148d60fd
Daniel Vaz <dcva9565@hotmail.com>
2022-04-17 16:08:35 -0500

    creamos una nueva instancia del modelo Paciente en pacienteController, esto para traer todos los datos del modelo

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -1,4 +1,9 @@
-const agregarPaciente = (req, res) => {};
+import Paciente from "../models/Paciente.js";
+
+const agregarPaciente = (req, res) => {
+  const paciente = new Paciente(req.body);
+  console.log(paciente);
+};
 const obtenerPaciente = (req, res) => {};
 
 export { agregarPaciente, obtenerPaciente };
